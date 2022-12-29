726295f85f81ff34a2adcfcb2e05a4c0998cf01e
Richard Caringal <richard.caringal23@gmail.com>
2022-12-29 16:01:40 +0800

    fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href={`${process.env.NEXT_PUBLIC_FAVICON}/favicon.ico`} />
+      </Head>
       <body>
         <Main />
         <NextScript />

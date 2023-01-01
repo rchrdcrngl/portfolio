ede76becc0928716d8ef39c3e240273a5d0a8e88
Richard Caringal <richard.caringal23@gmail.com>
2023-01-01 12:49:05 +0800

    fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,18 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
-export default function FourOhFour() {
+export default function Error404() {
     return (
         <>
             <Head>
                 <title>404 - Page Not Found</title>
             </Head>
-            <div className='flex justify-center items-center h-screen'>
-                <div className='font-bold text-lg bg-slate-900'>404</div>
-                <div>The page you are requesting does not exist</div>
-                <Link href={'/'}>Go back home.</Link>
+            <div className='flex justify-center items-center h-screen bg-slate-900 text-white'>
+                <div className='justify-center items-center w-20'>
+                    <div className='font-bold text-lg'>404</div>
+                    <div>The page you are requesting does not exist</div>
+                    <Link href={'/'}>Go back home.</Link>
+                </div>
             </div>
         </>
     )

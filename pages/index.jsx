f1dbb6c019377cd729eeaf97df6c0398492bd4d1
Richard Caringal <richard.caringal23@gmail.com>
2023-01-04 18:44:57 +0800

    remove projects, add data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ projects }) {
   return (
     <>
       <Head>
         <title>RCHRD CRNGL</title>
       </Head>
-      <NavBar/>
+      <NavBar />
       <main className={styles.main}>
         Hello, I'm Richard Maru Caringal
       </main>
+      <section id='projects'>
+        {JSON.stringify(projects)}
+      </section>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const raw = await fs.readFile(path.join(process.cwd(), "./data.json"), { encoding: "utf-8" });
+  const parsed = await JSON.parse(raw)
+  const partialData = parsed.map(({ title, slug, images }) => ({ title, slug, images }))
+  return { props: { projects: partialData } };
 }

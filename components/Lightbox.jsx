a26ea31b5489bf4b8ddba2b070a9b9d8deb87091
rchrdcrngl <richard.caringal23@gmail.com>
2023-07-22 00:13:19 +0800

    put gallery inside lightbox

--- components/Lightbox.jsx
+++ components/Lightbox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 const Lightbox = ({ images, selectedIndex }) => {
   const [showLightbox, setShowLightbox] = useState(false);
@@ -15,54 +15,54 @@ const Lightbox = ({ images, selectedIndex }) => {
   };
 
   useEffect(() => {
+    console.log(selectedIndex);
     if (selectedIndex && selectedIndex >= 0) {
-        setSelectedImage(images[selectedIndex]);
+      openLightbox(images[selectedIndex].url);
     }
-  }, [ selectedIndex ]);
+  }, [selectedIndex]);
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {images.map((url, index) => (
-        <div key={index} onClick={() => openLightbox(url)}>
+    showLightbox && (
+      <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-black bg-opacity-75">
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((img, index) => (
+            <div key={index} onClick={() => openLightbox(img.url)}>
+              <img
+                src={img.url}
+                alt={`Image ${index + 1}`}
+                className="cursor-pointer w-full h-auto"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="relative">
           <img
-            src={url}
-            alt={`Image ${index + 1}`}
-            className="cursor-pointer w-full h-auto"
+            src={selectedImage}
+            alt="Selected Image"
+            className="max-h-full"
           />
-        </div>
-      ))}
-
-      {showLightbox && (
-        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-black bg-opacity-75">
-          <div className="relative">
-            <img
-              src={selectedImage}
-              alt="Selected Image"
-              className="max-h-full"
-            />
-            <button
-              className="absolute top-4 right-4 p-2 text-white bg-black rounded-full hover:bg-opacity-50"
-              onClick={closeLightbox}
+          <button
+            className="absolute top-4 right-4 p-2 text-white bg-black rounded-full hover:bg-opacity-50"
+            onClick={closeLightbox}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
-      )}
-    </div>
+      </div>
+    )
   );
 };
 

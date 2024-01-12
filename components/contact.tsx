79afe211da8d85b26c8384110ada080605ff6c9a
rchrdcrngl <richard.caringal23@gmail.com>
2024-01-13 03:40:35 +0800

    update contacts

--- components/contact.tsx
+++ components/contact.tsx
@@ -17,7 +17,7 @@ export default function Contact({ contacts }: ContactsProps) {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-20 sm:mb-28 w-screen px-24"
+      className="mb-20 sm:mb-28 w-screen px-0 md:px-24"
       initial={{
         opacity: 0,
       }}
@@ -32,7 +32,7 @@ export default function Contact({ contacts }: ContactsProps) {
       }}
     >
       <div className="grid grid-cols-3 grid-rows-2">
-        <div className="font-clash text-xl md:text-5xl border col-span-2 flex items-end p-8 bg-primary-500 text-primary-100">Contact me @</div>
+        <div className="font-clash text-xl md:text-5xl border row-span-2 md:row-span-1 col-span-1 md:col-span-2 flex md:items-end p-8 bg-primary-500 text-primary-100">Contact me at</div>
         {contacts.map((contact,index)=>(
           <Link href={contact.url} className={`border w-full h-52 flex justify-center items-center text-xl md:text-3xl bg-secondary-${Math.max(500-index*100,100)} text-accent-${Math.min(500+index*100,900)}`}>{contact.name}</Link>
         ))}

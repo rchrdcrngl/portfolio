f2aebd34769523510eb35ca0f1ab6cbff20193bd
rchrdcrngl <richard.caringal23@gmail.com>
2024-01-14 01:35:08 +0800

    add tech stack section

---
+++ components/tech-stack.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+type TechStackProps = {
+  technologies: string[];
+};
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+export default function TechStack({ technologies }: TechStackProps) {
+  return (
+    <section
+      id="technologies"
+      className="relative mb-28 h-full scroll-mt-28 sm:mb-40"
+    >
+      <div className="flex flex-col md:flex-row w-full h-max">
+        <div className="relative w-full md:w-2/5 px-3 h-full flex items-center justify-center mb-4 md:mb-0">
+          <h1
+            className="font-clash font-bold text-3xl md:text-5xl">
+            Technologies Used
+          </h1>
+        </div>
+        <div className="relative w-full md:w-3/5 h-auto flex justify-center items-center">
+          <ul className="flex flex-wrap justify-start gap-2 text-lg md:text-2xl">
+            {technologies.map((tech, index) => (
+              <motion.li
+                className="px-5 py-1 md:py-3 bg-white borderBlack rounded-full dark:bg-white/10 dark:text-white/80"
+                key={index}
+                variants={fadeInAnimationVariants}
+                initial="initial"
+                whileInView="animate"
+                custom={index}
+              >
+                {tech}
+              </motion.li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}

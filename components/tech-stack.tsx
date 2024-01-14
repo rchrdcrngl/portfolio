1312fbd0ed7a361090a257bb81a137f222111d39
rchrdcrngl <richard.caringal23@gmail.com>
2024-01-14 12:06:48 +0800

    increase margin tech stack section

--- components/tech-stack.tsx
+++ components/tech-stack.tsx
@@ -24,7 +24,7 @@ export default function TechStack({ technologies }: TechStackProps) {
   return (
     <section
       id="technologies"
-      className="relative mb-28 h-full scroll-mt-28 sm:mb-40"
+      className="relative my-28 h-full scroll-mt-28 sm:my-40"
     >
       <div className="flex flex-col md:flex-row w-full h-max">
         <div className="relative w-full md:w-2/5 px-3 h-full flex items-center justify-center mb-4 md:mb-0">

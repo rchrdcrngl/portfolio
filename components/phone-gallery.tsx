29a8848c8e81a5f21a5cbc7eb8954f058668c876
rchrdcrngl <richard.caringal23@gmail.com>
2024-01-15 18:52:29 +0800

    use vmin and insets to position elements

--- components/phone-gallery.tsx
+++ components/phone-gallery.tsx
@@ -15,14 +15,14 @@ export default function PhoneGallery({ images }: PhoneGalleryProps) {
     target: sectionRef,
     offset: ["start end", "end end"],
   });
-  const x = useTransform(scrollYProgress, [0, 1], ["50%", "-95%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["95%", "-95%"]);
   return (
     <section ref={sectionRef} className="relative h-[300vh]">
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
         <div className="absolute h-full w-full px-4 flex justify-center items-center">
             <h1 className="text-6xl md:text-9xl font-clash font-bold text-outline opacity-20 uppercase">Sample Screenshots</h1>
         </div>
-        <motion.div style={{ x }} className="flex gap-8">
+        <motion.div style={{ x }} className="flex gap-6">
           {images.map((image, index) => {
             return <Card image={image} key={index} />;
           })}
@@ -36,22 +36,25 @@ const Card = ({ image, key }:{ image:ProjectImage, key:number }) => {
     return (
       <div
         key={key}
-        className="group relative h-max w-[90vw] md:w-[calc(100vw/7)] px-4 py-1 overflow-hidden 
+        className="group relative h-[65vmin] w-[35vmin] overflow-hidden flex justify-center items-center
                    transition-transform duration-300 hover:scale-110"
       >
-        <img src={"phone.webp"} className="absolute inset-0 h-full w-full z-[5]" />
-        <div className="relative h-full w-full px-2 md:px-0 py-4 md:py-3 z-0 rounded-[4rem] md:rounded-[2rem] overflow-hidden">
-            <div className="z-[3] px-4 absolute h-full w-full opacity-0 group-hover:opacity-100 flex justify-center items-center group-hover:bg-teal-500">
-                <p className="text-white font-clash font-bold text-xl">{image.description}</p>
-            </div>
-            <Image
-            height={0}
-            width={0}
-            sizes="100%"
-            src={image.url}
-            className="relative h-full w-full object-cover object-center"
-            alt={image.description||"Project gallery image"}
-            />
+        <div className="relative h-full w-auto">
+          <img src={"phone.webp"} className="relative h-full w-auto z-[5]" />
+          <div className="absolute top-[1.95vmin] left-[2.35vmin] right-[2.35vmin] bottom-[1.95vmin] rounded-[1.5vmin] z-0 overflow-hidden">
+              <Image
+              height={0}
+              width={0}
+              sizes="100%"
+              src={image.url}
+              className="relative h-full w-full object-cover object-center"
+              alt={image.description||"Project gallery image"}
+              />
+              <div className="absolute z-[3] pt-6 px-14 pb-24 h-full w-full opacity-0 group-hover:opacity-100 
+                              flex justify-center items-center group-hover:bg-teal-500 inset-0">
+                  <p className="text-white font-clash font-bold text-xl">{image.description}</p>
+              </div>
+          </div>
         </div>
       </div>
     );
